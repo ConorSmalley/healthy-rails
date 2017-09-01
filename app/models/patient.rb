1d34c5e7503e0b66ab4ba84cfc1e24740935cff1
Conor Smalley <conorsmalley93@gmail.com>
2017-09-01 12:14:36 +0000

    Added Arel Query

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -26,6 +26,28 @@ class Patient < ActiveRecord::Base
             # end
             str = str + "<br>" + "<br>"
         end
+        
+        Patient.select(
+          [
+            Patient.arel_table[:id], Patient.arel_table[:fullname], Medication.arel_table[:formulatedName], MedicationDrug.arel_table[:drugMg], MedicationDrug.arel_table[:drugMl].as('MedicationDrug'), Drug.arel_table[:drug]
+          ]
+        ).where(Patient.arel_table[:id].eq(1)).joins(
+          Patient.arel_table.join(DrugAdministration.arel_table).on(
+            Patient.arel_table[:id].eq(DrugAdministration.arel_table[:patient_id])
+          ).join_sources
+        ).joins(
+          Patient.arel_table.join(Medication.arel_table).on(
+            DrugAdministration.arel_table[:medication_id].eq(Medication.arel_table[:id])
+          ).join_sources
+        ).joins(
+          Patient.arel_table.join(MedicationDrug.arel_table).on(
+            MedicationDrug.arel_table[:Medication_id].eq(Medication.arel_table[:id])
+          ).join_sources
+        ).joins(
+          Patient.arel_table.join(Drug.arel_table).on(
+            MedicationDrug.arel_table[:drug_id].eq(Drug.arel_table[:id])
+          ).join_sources
+        )
         # puts str
         return str
         # return "test"
