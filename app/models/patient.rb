bcf89912e47a051cfd4e137b883b7f3e63b6871b
Conor Smalley <conorsmalley93@gmail.com>
2017-09-04 21:05:22 +0000

    retrieved medication list for past 24 hours using Arel_Table

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -16,7 +16,45 @@ class Patient < ActiveRecord::Base
         return str
     end
     def drugs
-        return "drugs"
+        str = ""
+    # SELECT 
+    #     drugs.drug,
+    #     sum(medication_drugs.drugMg / medication_drugs.drugMl) as 'tst'
+    # FROM 
+    # drug_administrations 
+    # inner join medications on drug_administrations.medication_id = medications.id
+    # inner join medication_drugs on medication.id = medication_drugs.medication_id
+    # inner join drugs on medication_drugs.drug_id = drugs.id
+    # where drug_administrations.dt >= 'start'
+    # group by
+    # drugs.drug
+        abc = DrugAdministration.select(
+          [
+            Drug.arel_table[:drug], Arel::Nodes::NamedFunction.new(
+              'sum', [
+                MedicationDrug.arel_table[:drugMg] / MedicationDrug.arel_table[:drugMl] * DrugAdministration.arel_table[:quantity]
+              ]
+            ).as('tst')
+          ]
+        ).where(DrugAdministration.arel_table[:dt].gteq(DateTime.now-48.hours)).joins(
+          DrugAdministration.arel_table.join(Medication.arel_table).on(
+            DrugAdministration.arel_table[:medication_id].eq(Medication.arel_table[:id])
+          ).join_sources
+        ).joins(
+          DrugAdministration.arel_table.join(MedicationDrug.arel_table).on(
+            Medication.arel_table[:id].eq(MedicationDrug.arel_table[:medication_id])
+          ).join_sources
+        ).joins(
+          DrugAdministration.arel_table.join(Drug.arel_table).on(
+            MedicationDrug.arel_table[:drug_id].eq(Drug.arel_table[:id])
+          ).join_sources
+        ).group(Drug.arel_table[:drug])
+
+        abc.each do |da|
+            # puts "med " + da.medication.medication
+            str = str + da.drug + " " + da.tst.to_s  + "Mg<br>"
+        end
+        return str
     end
     def medOD
         str = ""
@@ -72,13 +110,6 @@ class Patient < ActiveRecord::Base
             str = str + "<br>"
         end
         return str
-    end
-    def test
-        query = <<-SQL
-          SELECT patients.*,
-          FROM  patients
-        SQL
-        self.find_by_sql(query)
     end
     def isOD
         str = ""
